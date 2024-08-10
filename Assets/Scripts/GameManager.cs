c330bab03418e85215a93091a44c31b42b6b01d6
Aidan Westphal <93747131+AidanWestphal@users.noreply.github.com>
2024-08-10 00:07:50 -0400

    Commit #2 for file strucutre

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public static class Layers
-{
-    public const int UI = 5;
-    public const int PLAYER = 6;
-    public const int ENEMY = 7;
-    public const int ITEM = 8;
-    public const int MAP = 9;
-}
-
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public static class Layers
+{
+    public const int UI = 5;
+    public const int PLAYER = 6;
+    public const int ENEMY = 7;
+    public const int ITEM = 8;
+    public const int MAP = 9;
+}
